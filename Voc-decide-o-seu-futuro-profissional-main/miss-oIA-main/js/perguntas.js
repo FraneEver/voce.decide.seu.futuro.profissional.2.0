79ce086a372c279e92948c17664ea996f9d4fc85
FraneEver <110260313+FraneEver@users.noreply.github.com>
2024-08-22 11:00:30 -0300

    Add files via upload

---
+++ Voc-decide-o-seu-futuro-profissional-main/miss-oIA-main/js/perguntas.js
@@ -0,0 +1,155 @@
+
+export const perguntas = [
+    {
+        enunciado: "Escolha uma área de foco principal para você na vida futura",
+        alternativas: [
+            {
+                texto: "Carreira Profissional",
+                afirmacao: 
+                [
+                "Buscar uma promoção na empresa atual,",
+                "Mudar de carreira para seguir um novo sonho,"] ,
+                proxima:1,
+            },
+            {
+                texto: "Desenvolvimento Pessoal",
+                afirmacao: [
+                "Dedicar-se ao aprendizado contínuo (cursos, workshops),",
+                "Focar no autoconhecimento e práticas de bem-estar (meditação, terapia),"],
+                proxima:2,
+            }
+        ]
+    },
+    {
+        enunciado: "Como você lida com oportunidades de mudança?",
+        alternativas: [
+            {
+                texto: "Abraçar mudanças e buscar novas oportunidades",
+                afirmacao: [
+                    "mudar para uma nova cidade,", 
+                    "assumir projetos desafiadores,"],
+                    proxima:3,
+            },
+            {
+                texto: "Preferir a estabilidade e aprimorar o que já está estabelecido",
+                afirmacao: [
+                    "fortalecer relações profissionais e pessoais,",
+                    " investir em melhorias no ambiente de trabalho ou moradia atual,"],
+                    proxima:4,
+            }
+        ]
+    },
+    {
+        enunciado: "Qual abordagem você tem para a saúde e bem-estar no futuro?",
+        alternativas: [
+            {
+                texto: "Adotar uma rotina de exercícios rigorosa e uma dieta equilibrada",
+                afirmacao: [
+                    "praticar esportes regularmente,", 
+                    " consultar nutricionistas e seguir planos de alimentação,"],
+                    proxima:5,
+            },
+            {
+                texto: "Focar em um equilíbrio mais flexível entre saúde e prazer",
+                afirmacao: [
+                    "participar de atividades ao ar livre e hobbies que tragam prazer,",
+                    " permitir-se indulgências ocasionais na dieta,"],
+                    proxima:6,
+            }
+        ]
+    },
+    {
+        enunciado: "Como você planeja suas finanças futuras?",
+        alternativas: [
+            {
+                texto: "Ser financeiramente conservador e focar em poupança e investimentos seguros",
+            afirmacao: [
+                "investir em imóveis ou fundos de renda fixa,", 
+                " manter uma reserva financeira para emergências,"],
+                proxima:7,
+            },
+            {
+                texto: "Adotar uma abordagem mais arriscada com potencial para maiores retornos",
+                afirmacao:[ 
+                    "investir em ações e startups,",
+                    " explorar novas oportunidades de negócios,"],
+                    proxima:8,
+            }
+        ]
+    },
+    {
+        enunciado: "Qual é a visão de você sobre relacionamentos pessoais no futuro?",
+        alternativas: [
+            {
+                texto: "Priorizar a construção de uma família sólida e tradicional",
+                afirmacao: [
+                    "casar-se e ter filhos,",
+                    " focar em criar um ambiente familiar estável,"],
+                    proxima:9,
+            },
+            {
+                texto: "Valorizar a liberdade e experiências variadas nos relacionamentos",
+                afirmacao: [
+                    "manter relacionamentos alternativos,",
+                    " viajar e conhecer novas culturas com amigos e parceiros,"],
+                    proxima:10,
+            }
+        ]
+    },
+    {
+        enunciado: "Qual é o papel de você na comunidade ou no mundo?",
+        alternativas: [
+            {
+                texto: "Envolver-se ativamente em causas sociais e voluntariado",
+                afirmacao: [
+                    " trabalhar com ongs ou projetos comunitários,",
+                    " dedicar-se a causas ambientais ou sociais,"],
+                proxima:11,
+            },
+            {
+                texto: "Contribuir de forma mais passiva, focando no impacto pessoal",
+                afirmacao: [
+                    "doar para caridades e apoiar causas à distância,", 
+                    "influenciar positivamente seu círculo próximo,"],
+                    proxima:12,
+            }
+        ]
+    },
+    {
+        enunciado: "Como você lida com o aprendizado e o crescimento contínuos?",
+        alternativas: [
+            {
+                texto: "Buscar sempre novas oportunidades de aprendizado formal",
+                afirmacao: [
+                    "fazer mestrados, doutorados ou especializações e", 
+                    " participar de conferências e seminários e"],
+                    proxima:13,
+            },
+            {
+                texto: "Aprender através da experiência e prática",
+                afirmacao: [
+                    "realizar viagens de imersão cultural e",
+                    "participar de projetos e desafios práticos e"],
+                    proxima:14,
+                            }
+        ]
+    },
+    {
+        enunciado: "Qual abordagem você usa para os desafios e contratempos?",
+        alternativas: [
+            {
+                texto: "Enfrentar desafios de forma proativa e com resiliência",
+                afirmacao: [
+                    "desenvolver planos de ação detalhados.",
+                     "buscar apoio em redes de suporte."]
+            },
+            {
+                texto: "Adaptar-se aos desafios e buscar soluções criativas",
+                afirmacao: [
+                    "encontrar formas inovadoras de contornar problemas.", 
+                    "aceitar mudanças e ajustar-se rapidamente."]
+
+            }
+        ]
+    }
+];
